5921b471e429c1ac0e7db09ba4ce088030836dcc
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-02 21:50:12 +0600

    Add visitor pattern

---
+++ Patterns/Visitor/Animal/Animal.cs
@@ -0,0 +1,34 @@
+﻿namespace Visitor;
+
+public interface IAnimal
+{
+    void Accept(IVisitor visitor);
+}
+
+public class Cat : IAnimal
+{
+    public void Accept(IVisitor visitor) => visitor.Do(this);
+}
+
+public class Dog : IAnimal
+{
+    public void Accept(IVisitor visitor) => visitor.Do(this);
+}
+
+public interface IVisitor
+{
+    void Do(Cat cat);
+    void Do(Dog dog);
+}
+
+class Move : IVisitor
+{
+    public void Do(Cat cat) => Console.WriteLine(cat.GetType().Name + " " + GetType().Name);
+    public void Do(Dog dog) => Console.WriteLine(dog.GetType().Name + " " + GetType().Name);
+}
+
+class Speak : IVisitor
+{
+    public void Do(Cat cat) => Console.WriteLine(cat.GetType().Name + " " + GetType().Name);
+    public void Do(Dog dog) => Console.WriteLine(dog.GetType().Name + " " + GetType().Name);
+}
---
+++ Patterns/Visitor/Program.cs
@@ -0,0 +1,24 @@
+﻿using Visitor;
+
+// Суть шаблона Visitor - создание двух иерархий классов
+// объектов и посетителей, посетители выполняют роль
+// классов описывающих операции необходимые для выполнения
+// с объектами, из-за чего нет необходимости их изменения
+
+List<IAnimal> animals =
+[
+    new Cat(),
+    new Dog(),
+    new Cat(),
+    new Cat()
+];
+
+var move = new Move();
+foreach (IAnimal animal in animals)
+    animal.Accept(move);
+
+var speak = new Speak();
+foreach (IAnimal animal in animals)
+    animal.Accept(speak);
+
+Console.Read();
