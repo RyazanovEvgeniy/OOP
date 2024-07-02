b5dcb0f5c5944d57867b1a62b8fd9be5442a389f
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-02 21:25:18 +0600

    Add TemplateMethod pattern

---
+++ Patterns/TemplateMethod/Template/Template.cs
@@ -0,0 +1,45 @@
+﻿namespace TemplateMethod;
+
+// Класс человека
+abstract class Human
+{
+    // Создаем структура алгоритма в шаблонном методе
+    public void TemplateMethod()
+    {
+        GetInformation();
+        MakeDecision();
+        Work();
+    }
+
+    // Абстрактные методы,
+    // которые должны быть в реализованы у производных классов.
+    // Метод поиска информации
+    protected abstract void GetInformation();
+
+    // Метод принятия решения
+    protected abstract void MakeDecision();
+
+    // Метод работы
+    protected abstract void Work();
+}
+
+class SmartHuman : Human
+{
+    // Метод поиска информации
+    protected override void GetInformation()
+    {
+        Console.WriteLine("SmartHuman.GetInformation()");
+    }
+
+    // Метод принятия решения
+    protected override void MakeDecision()
+    {
+        Console.WriteLine("SmartHuman.MakeDecision()");
+    }
+
+    // Метод работы
+    protected override void Work()
+    {
+        Console.WriteLine("SmartHuman.Work()");
+    }
+}
