2a1551c16bff2bf9e60c39c560eb253850e52b5a
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-09 21:05:04 +0600

    Add Composite

---
+++ Patterns/Structural/Composite/Pattern/Component.cs
@@ -0,0 +1,94 @@
+﻿namespace Pattern;
+
+// Класс компонент
+abstract class Component
+{
+    public Component() { }
+
+    // Абстрактный метод сбора информации, который должны реализовать наследники класса
+    public abstract string GetInfo();
+
+    // Метод добавления нового элемента, если компонент составной
+    public virtual void Add(Component component)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Метод удаления элемента, если компонент составной
+    public virtual void Remove(Component component)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Метод возвращающий заключение, составной ли компонент (то есть может ли содержать другие компоненты)
+    public virtual bool IsComposite()
+    {
+        return true;
+    }
+}
+
+// Класс листа
+class Leaf : Component
+{
+    // Замещаем метод получения информации о компоненте
+    public override string GetInfo()
+    {
+        return "Leaf";
+    }
+
+    // Метод возвращающий заключение, составной ли компонент (то есть может ли содержать другие компоненты)
+    public override bool IsComposite()
+    {
+        return false;
+    }
+}
+
+// Класс ветки
+class Branch : Component
+{
+    // Выделяем память под лист компонентов
+    protected List<Component> component = new List<Component>();
+
+    // Замещаем метод добавления компонента
+    public override void Add(Component component)
+    {
+        this.component.Add(component);
+    }
+
+    // Замещаем метод удаления компонента
+    public override void Remove(Component component)
+    {
+        this.component.Remove(component);
+    }
+
+    // Замещаем метод получения информации о компоненте
+    public override string GetInfo()
+    {
+        string result = "Branch(";
+
+        // Для каждого компонента в листе компонентов, собираем информацию о их компонентах =)))
+        int i = 0;
+        foreach (Component component in this.component)
+        {
+            // И записываем в возвращаемую строку
+            result += component.GetInfo();
+
+            // Костыль для вывода плюсов
+            if (i < this.component.Count - 1)
+                result += "+";
+            i++;
+        }
+
+        //Альтернативный способо вывода
+        /*
+        for (int i = 0; i < this.component.Count; i++)
+        {
+            result += this.component[i].GetInfo();
+
+            if (i < this.component.Count - 1)
+                result += "+";
+        }*/
+
+        return result + ")";
+    }
+}
---
+++ Patterns/Structural/Composite/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pattern;
+
+// Суть шаблона Компоновщик (Composite) - создание абстрактного класса,
+// в данном примере класса Component,
+// с какими либо общими методами, в данном случае GetInfo,
+// что позволяет сформировать из объектов наследуемых от Component древовидную структуру.
+// В дальнейшем это позволяет работать с этой структурой как с единым объектом
+
+// Создаем первую ветку, так как она самая первая, то по факту это дерево;)
+Branch tree = new();
+
+// На ней вырастает первая ветка с двумя листочками
+Branch branch1 = new();
+branch1.Add(new Leaf());
+branch1.Add(new Leaf());
+tree.Add(branch1);
+
+// Еще ветка с одним листочком и веткой без листьев
+Branch branch2 = new();
+branch2.Add(new Leaf());
+Branch branch3 = new();
+branch2.Add(branch3);
+tree.Add(branch2);
+
+// Выводим то, что произрастает
+Console.WriteLine($"Tree: {tree.GetInfo()}\n");
+
+Console.Read();
