0dea2d090649966d7b2e0d8985e62b0fc7489be5
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-12 20:20:06 +0600

    Add AdapaterAssociation

---
+++ Patterns/Structural/AdapterAssociation/Pattern/Adapter.cs
@@ -0,0 +1,20 @@
+﻿namespace Pattern;
+
+// Интерфейс необходимый для реализации
+interface IMachine
+{
+    void Work();
+}
+
+// Класс адаптер, которому необходимо реализовать интерфейс IMachine
+class Adapter : IMachine
+{
+    // Создаем экземпляр класса Machine
+    readonly Machine machine = new();
+
+    // Реализуем наш интерфейс, с помощью экземпляра класса
+    public void Work()
+    {
+        machine.DoWork();
+    }
+}
