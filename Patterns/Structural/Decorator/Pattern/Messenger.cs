21ad8abd9726071a31538ec935817d6cb30adda4
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-12 20:55:23 +0600

    Add Decorator

---
+++ Patterns/Structural/Decorator/Pattern/Messenger.cs
@@ -0,0 +1,135 @@
+﻿namespace Pattern;
+
+// Наш абстрактный класс Messenger
+public abstract class Messenger
+{
+    // Потомки которого должны будут реализовать методы SendAlarm и SendInformation
+    public abstract string SendAlarm();
+    public abstract string SendInformation();
+}
+
+// Первый вариант вывода сообщений
+class MessengerStandart1 : Messenger
+{
+    // Реализуем метод SendAlarm
+    public override string SendAlarm()
+    {
+        return "MessengerStandart1 Alarm";
+    }
+
+    // Реализуем метод SendInformation
+    public override string SendInformation()
+    {
+        return "MessengerStandart1 Information";
+    }
+}
+
+// Второй вариант вывода сообщений
+class MessengerStandart2 : Messenger
+{
+    // Реализуем метод SendAlarm
+    public override string SendAlarm()
+    {
+        return "MessengerStandart2 Alarm";
+    }
+
+    // Реализуем метод SendInformation
+    public override string SendInformation()
+    {
+        return "MessengerStandart2 Information";
+    }
+}
+
+// Класс Decorator
+abstract class Decorator : Messenger
+{
+    // Поле куда запишем, какой Messenger использует наш декоратор
+    protected Messenger messenger;
+
+    // Конструктор класса
+    public Decorator(Messenger messenger)
+    {
+        this.messenger = messenger;
+    }
+
+    // Метод установки поля messenger
+    public void SetMessenger(Messenger messenger)
+    {
+        this.messenger = messenger;
+    }
+
+    // Замещаем метод SendAlarm
+    public override string SendAlarm()
+    {
+        // Проверим не пустой ли объект
+        if (this.messenger != null)
+            // Реализуем через наш messenger
+            return this.messenger.SendAlarm();
+        else
+            return string.Empty;
+    }
+
+    // Замещаем метод SendInformation
+    public override string SendInformation()
+    {
+        // Проверим не пустой ли объект
+        if (this.messenger != null)
+            // Реализуем через наш messenger
+            return this.messenger.SendAlarm();
+        else
+            return string.Empty;
+    }
+}
+
+// Ну и три варианта декоратора
+// в которых замещаются методы SendAlarm и SendInformation
+class WhatsApp : Decorator
+{
+    public WhatsApp(Messenger messenger) : base(messenger)
+    {
+    }
+
+    public override string SendAlarm()
+    {
+        return $"WhatsApp({base.SendAlarm()})";
+    }
+
+    public override string SendInformation()
+    {
+        return $"WhatsApp({base.SendAlarm()})";
+    }
+}
+
+class Viber : Decorator
+{
+    public Viber(Messenger messenger) : base(messenger)
+    {
+    }
+
+    public override string SendAlarm()
+    {
+        return $"Viber({base.SendAlarm()})";
+    }
+
+    public override string SendInformation()
+    {
+        return $"Viber({base.SendAlarm()})";
+    }
+}
+
+class Telegram : Decorator
+{
+    public Telegram(Messenger messenger) : base(messenger)
+    {
+    }
+
+    public override string SendAlarm()
+    {
+        return $"Telegram({base.SendAlarm()})";
+    }
+
+    public override string SendInformation()
+    {
+        return $"Telegram({base.SendAlarm()})";
+    }
+}
---
+++ Patterns/Structural/Decorator/Program.cs
@@ -0,0 +1,51 @@
+﻿using Pattern;
+
+// Суть шаблона Декоратор - заключается в создании абстрактного класса,
+// в данном примере класса Messenger,
+// который реализуется в классах наследниках messengerStandart1 и messengerStandart2,
+// и создании класса Decorator, который принимает в параметрах объект того же абстрактного типа,
+// который наследует сам Decorator. 
+// Целью данных манипуляций является - возможность динамически добавлять существующим объектам новую функциональность.
+// Например добавить возможность нашему объекту messengerStandart1, (типа) отправлять сообщение в WhatsApp, Viber и тд.
+
+// Создаем объект MessengerStandart1 и смотрим его работу
+MessengerStandart1 messengerStandart1 = new();
+Console.WriteLine($"messengerStandart1.SendAlarm() == {messengerStandart1.SendAlarm()}");
+Console.WriteLine($"messengerStandart1.SendInformation() == {messengerStandart1.SendInformation()}\n");
+
+// Создаем объект MessengerStandart2 и смотрим его работу
+MessengerStandart2 messengerStandart2 = new();
+Console.WriteLine($"messengerStandart2.SendAlarm() == {messengerStandart2.SendAlarm()}");
+Console.WriteLine($"messengerStandart2.SendInformation() == {messengerStandart2.SendInformation()}\n");
+
+// Создаем объект WhatsApp(messengerStandart1) и апкастим его к Messenger, смотрим как работает
+Messenger messenger = new WhatsApp(messengerStandart1);
+Console.WriteLine($"WhatsApp(messengerStandart1) messenger.SendAlarm() == {messenger.SendAlarm()}");
+Console.WriteLine($"WhatsApp(messengerStandart1) messenger.SendInformation() == {messenger.SendInformation()}\n");
+
+// Тоже самое в Viber, только с messengerStandart2
+messenger = new Viber(messengerStandart2);
+Console.WriteLine($"Viber(messengerStandart2) messenger.SendAlarm() == {messenger.SendAlarm()}");
+Console.WriteLine($"Viber(messengerStandart2) messenger.SendInformation() == {messenger.SendInformation()}\n");
+
+// Downcast и проверка
+Viber viber = (Viber)messenger;
+Console.WriteLine($"Viber(messengerStandart2) messenger.SendAlarm() == {messenger.SendAlarm()}");
+Console.WriteLine($"Viber(messengerStandart2) messenger.SendInformation() == {messenger.SendInformation()}\n");
+
+// Проверяем работает ли смена стандарта
+viber.SetMessenger(messengerStandart1);
+Console.WriteLine($"Viber(messengerStandart1) messenger.SendAlarm() == {messenger.SendAlarm()}");
+Console.WriteLine($"Viber(messengerStandart1) messenger.SendInformation() == {messenger.SendInformation()}\n");
+
+// Ну чисто поржать
+messenger = new Telegram(messenger);
+messenger = new WhatsApp(messenger);
+messenger = new Telegram(messenger);
+messenger = new Viber(messenger);
+messenger = new Viber(messenger);
+messenger = new Telegram(messenger);
+Console.WriteLine($"messenger.SendAlarm() == {messenger.SendAlarm()}");
+Console.WriteLine($"messenger.SendInformation() == {messenger.SendInformation()}\n");
+
+Console.Read();
