160f482d3611a62bcc73a61e8a3a25ed6f0bc891
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-07-12 20:36:47 +0600

    Add AdapterInheritance

---
+++ Patterns/Structural/AdapterInheritance/Pattern/Adapter.cs
@@ -0,0 +1,17 @@
+﻿namespace Pattern;
+
+// Интерфейс необходимый для реализации
+interface IMachine
+{
+    void Work();
+}
+
+// Класс адаптер, которому необходимо реализовать интерфейс IMachine
+class Adapter : Machine, IMachine
+{
+    // Реализуем наш интерфейс, с помощью базового класса
+    public void Work()
+    {
+        DoWork();
+    }
+}
---
+++ Patterns/Structural/AdapterInheritance/Pattern/Machine.cs
@@ -0,0 +1,14 @@
+﻿namespace Pattern;
+
+// Класс, недоступный для изменения
+class Machine
+{
+    private int counter;
+
+    // И его работа
+    public void DoWork()
+    {
+        counter++;
+        Console.WriteLine("Work " + counter);
+    }
+}
